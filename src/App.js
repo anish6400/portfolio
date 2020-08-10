6925b7467bdff066580395ebd1bbc075c4024ad2
anish <anish@anish-ubunutu.hitronhub.home>
2020-08-10 11:08:34 -0700

    production build commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//pages
+import about from "./pages/about";
+import projects from "./pages/projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={about} />
+        <Route exact path="/projects" component={projects} />
+      </Switch>
+    </Router>
   );
 }
 
